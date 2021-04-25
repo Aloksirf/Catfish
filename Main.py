eed17fe850725f49ff940a0f64e67f684b661de5
Eekdeb <50443198+Eekdeb@users.noreply.github.com>
2021-04-25 18:49:57 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -5,13 +5,11 @@ import time
 def main():
     #The mission starts/inisialises
     logg = Logger() #Create a new instans of logger. Creates a new missionNr in the database
-    sendData = True #A boolian for if it needs to send data of not
     missionOnGoing = True #It will be in a while loop until the mission is starting
     while(missionOnGoing):
         #At the same time as the catfish is sailing to the next waypoint it will
         #try to send data that is not sent to the server
-        if(sendData):
-            dataSent = Client.sendData()
+        Client.sendData()
         
         #When the catfish has gotten to the waypoint
         atWaypoint = True #check if catfish arrived at waypoint 
@@ -26,5 +24,7 @@ def main():
             #tell the pixhawk that we are done and send it to the next waypoint
         
         #when we are done with a mission set the missionOnGoing to false
-        time.sleep(20) #set a time so the pi does not work to hard
+                print("reset")
+        time.sleep(1) #set a time so the pi does not work to hard
 main()
+
